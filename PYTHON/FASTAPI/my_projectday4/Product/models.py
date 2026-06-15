f405e939ccceee4dc97eb7493ce7f45f37e5ed6e
shruthika <shruthiruth21@gmail.com>
2026-06-15 15:04:08 +0530

    models - db tables

--- PYTHON/FASTAPI/my_projectday4/Product/models.py
+++ PYTHON/FASTAPI/my_projectday4/Product/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from .database import Base
 
 
-class Product():
+class Product(Base):
     __tablename__ = "products" #database model class 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
